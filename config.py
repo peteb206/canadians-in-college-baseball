f37cbbe5502d915c58375774606246be1432acb9
Pete Berryman <peteb206@gmail.com>
2022-12-15 01:07:45 -0600

    pass api key from secrets into python

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 # Canadians in College Hub Google Sheet
 # os.environ['GOOGLE_CLOUD_API_KEY'] = ''
-# with open('/Users/peteberryman/Downloads/canadians-in-college-baseball-32cfc8392a02.json') as f: # insert location of JSON file
+# with open('') as f: # insert location of JSON file
 #     os.environ['GOOGLE_CLOUD_API_KEY'] = f.read()
 google_spreadsheets = GoogleSpreadsheet(keyfile = os.environ.get('GOOGLE_CLOUD_API_KEY'))
 hub_spreadsheet = google_spreadsheets.spreadsheet(name = 'Canadians in College Baseball Hub')
