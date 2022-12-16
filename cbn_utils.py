12340e7c0af8d2309b6f6b95bb4be6bfbbfcab13
Pete Berryman <peteb206@gmail.com>
2022-12-16 13:21:19 -0600

    Squashed commit of the following:
    commit 0c2c415ffe5ab9e1628ceabf618bb534acb40b39
    Author: Pete Berryman <peteb206@gmail.com>
    Date:   Fri Dec 16 13:17:42 2022 -0600
    
        print response statuses
    
    commit 4933525650c3610f277dc77108218302717ca472
    Author: Pete Berryman <peteb206@gmail.com>
    Date:   Thu Dec 15 17:06:20 2022 -0600
    
        improvements
    
    commit dee7eca4effe8d90c8e9a83e7cc3899394cee6e2
    Author: Pete Berryman <peteb206@gmail.com>
    Date:   Thu Dec 15 01:13:40 2022 -0600
    
        re-require lxml and update other packages
    
    commit 80538f0c6f1d0a580e8325996ba0a458430813dd
    Author: Pete Berryman <peteb206@gmail.com>
    Date:   Thu Dec 15 01:10:22 2022 -0600
    
        function was renamed
    
    commit f37cbbe5502d915c58375774606246be1432acb9
    Author: Pete Berryman <peteb206@gmail.com>
    Date:   Thu Dec 15 01:07:45 2022 -0600
    
        pass api key from secrets into python
    
    commit cae8ffa5e91ff0a52d78339bd9ca6e062bcd3b2f
    Author: Pete Berryman <peteb206@gmail.com>
    Date:   Thu Dec 15 01:03:06 2022 -0600
    
        initial push

--- cbn_utils.py
+++ cbn_utils.py
@@ -1,17 +1,19 @@
 def check_arg_type(name='', value=None, value_type=None) -> bool:
     assert type(value) == value_type, f'"{name}" argument must be of type {value_type.__name__}, NOT {type(value).__name__}'
 
+
 def check_string_arg(name='', value='', allowed_values=[], disallowed_values=[]) -> bool:
     passes, message = True, ''
     if len(allowed_values):
         if value not in allowed_values:
             passes = False
-            message = f'"{name}" argument must be a str from the following list: {allowed_values}. '
+            message = f'"{name}" argument must be a str from the following list: {allowed_values}. "{value}" was provided. '
     if len(disallowed_values):
         if value in disallowed_values:
             passes = False
-            message += f'"{name}" argument must be a str NOT from the following list: {disallowed_values}.'
+            message += f'"{name}" argument must be a str NOT from the following list: {disallowed_values}. "{value}" was provided. '
     assert passes, message
+
 
 def check_list_arg(name='', values=[], allowed_values=[], disallowed_values=[]) -> bool:
     passes, message = True, ''
@@ -19,9 +21,9 @@ def check_list_arg(name='', values=[], allowed_values=[], disallowed_values=[]) 
         if len(allowed_values):
             if value not in allowed_values:
                 passes = False
-                message = f'"{name}" argument must be from the following list: {allowed_values}. '
+                message += f'"{name}" argument must be from the following list: {allowed_values}. "{value}" was provided. '
         if len(disallowed_values):
             if value in disallowed_values:
                 passes = False
-                message += f'"{name}" argument must NOT be from the following list: {disallowed_values}.'
+                message += f'"{name}" argument must NOT be from the following list: {disallowed_values}. "{value}" was provided. '
     assert passes, message
---
+++ scrape.py
@@ -0,0 +1,104 @@
+from config import hub_spreadsheet
+from model import School, Page
+import requests
+import pandas as pd
+from datetime import datetime
+import time
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def players():
+    last_run = 'Last updated: {}'.format(datetime.now().strftime("%B %d, %Y"))
+
+    schools_worksheet = hub_spreadsheet.worksheet('Schools')
+    schools_df = pd.DataFrame(schools_worksheet.get_all_records(), dtype=str)
+    schools_df = schools_df[schools_df['scrape'] == 'TRUE'].drop('scrape', axis=1).reset_index(drop=True)
+    schools_df['success'] = False
+
+    # Set up logging
+    index_col_length, school_col_length, players_col_length, canadians_col_length, roster_link_col_length = len(str(len(schools_df.index))), int(max(schools_df['school'].str.len())), 7, 9, 11
+    print()
+    print('Reading the rosters of {} schools...'.format(str(len(schools_df.index))))
+    print()
+    border_row = f'|{"-" * (index_col_length + 2)}|{"-" * (school_col_length + 2)}|{"-" * (players_col_length + 2)}|{"-" * (canadians_col_length + 2)}|{"-" * (roster_link_col_length + 2)}'
+    print(border_row)
+    print(f'| {"#".ljust(index_col_length)} | {"school".ljust(school_col_length)} | {"players".ljust(players_col_length)} | {"canadians".ljust(canadians_col_length)} | {"roster_link".ljust(roster_link_col_length)}')
+    print(border_row)
+
+    all_canadians, session = list(), requests.Session()
+
+    # Start timer
+    start_time = time.time()
+
+    for i, school_series in schools_df.iterrows():
+        school = School(
+            name = school_series['school'],
+            league = school_series['league'],
+            division = str(school_series['division']),
+            state = school_series['state'],
+            roster_page = Page(url=school_series['roster_link'], session = session)
+        )
+        try:
+            players = school.players()
+            canadians = [player for player in players if player.canadian]
+            all_canadians += canadians
+        except Exception as e:
+            print(f'ERROR: {school.name} - {school.roster_page.url} - {str(e)}')
+        print(f'| {str(i + 1).ljust(index_col_length)} | {school.name.ljust(school_col_length)} | {str(len(players)).ljust(players_col_length)} | {str(len(canadians)).ljust(canadians_col_length)} | {school.roster_page.url} {school.roster_page.status}')
+        schools_df.at[i, 'success'] = len(players) > 0
+        time.sleep(1)
+    print(border_row)
+
+    new_players_df = pd.DataFrame([canadian.to_dict() for canadian in all_canadians]).drop('canadian', axis=1)
+    new_players_df['positions'] = new_players_df['positions'].apply(lambda x: '/'.join(x)) # Convert list to "/" delimited string
+
+    # Get existing values
+    players_worksheet = hub_spreadsheet.worksheet('Players')
+    players_worksheet_values = players_worksheet.get_all_values()
+    players_worksheet_columns = players_worksheet_values[0]
+    existing_players_df = pd.DataFrame(players_worksheet_values[1:], columns = players_worksheet_columns, dtype=str) # Read existing values
+    existing_players_df = existing_players_df[existing_players_df['last_name'] != ''] # Ignore blank row
+    existing_players_df['positions'] = existing_players_df['positions'].str.upper() # INF is converted to inf
+
+    # Combine existing and new values based on success of scrape
+    existing_players_df = pd.merge(existing_players_df, schools_df, how = 'left', on = ['school', 'league', 'division', 'state'])
+    combined_players_df = pd.concat([existing_players_df[~existing_players_df['success'].fillna(False)], new_players_df], ignore_index = True).drop(['roster_link', 'success'], axis = 1)
+    combined_players_df.sort_values(by = ['last_name', 'first_name'], inplace = True)
+
+    # Output to Canadians in College Hub Google Sheet
+    players_worksheet.resize(2) # Delete existing data
+    players_worksheet.resize(3)
+    players_worksheet.insert_rows(combined_players_df[players_worksheet_columns].values.tolist(), row = 3) # Add updated data
+
+    # Last Run
+    diff_df = pd.merge(existing_players_df, combined_players_df, on = ['last_name', 'first_name', 'school'], how='outer', suffixes = ['_', ''], indicator = 'diff')[players_worksheet_columns + ['diff']]
+    diff_df = diff_df[diff_df['diff'] != 'both']
+    diff_df['diff'] = diff_df['diff'].apply(lambda x: 'dropped' if x == 'left_only' else 'added')
+
+    last_run_worksheet = hub_spreadsheet.worksheet('Last Run')
+    last_run_worksheet.resize(2) # Delete existing data
+    last_run_worksheet.resize(3)
+    blank_row = ['' for _ in range(len(players_worksheet_columns))]
+    last_run_data = [
+        blank_row,
+        [last_run] + ['' for _ in range(len(players_worksheet_columns) - 1)],
+        blank_row,
+        ['Added'] + ['' for _ in range(len(players_worksheet_columns) - 1)],
+        blank_row
+    ]
+    last_run_data += diff_df[diff_df['diff'] == 'added'][players_worksheet_columns].fillna('').values.tolist()
+    last_run_data += [
+        blank_row,
+        blank_row,
+        ['Dropped'] + ['' for _ in range(len(players_worksheet_columns) - 1)],
+        blank_row
+    ]
+    last_run_data += diff_df[diff_df['diff'] == 'dropped'][players_worksheet_columns].fillna('').values.tolist()
+    last_run_worksheet.insert_rows(last_run_data, row = 3) # Add updated data
+
+    # Print results
+    print()
+    print(f'{len(schools_df[schools_df["success"]].index)} successes... {len(schools_df[~schools_df["success"]].index)} empty rosters/failures...')
+    print()
+    print(f'--- Total time: {round((time.time() - start_time) / 60, 1)} minutes ---')
