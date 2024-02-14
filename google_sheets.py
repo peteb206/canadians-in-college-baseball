485e2232d4fac47b2427d2f9c116371e99e9a1e0
Pete Berryman <70476220+peteb206@users.noreply.github.com>
2024-02-13 22:26:47 -0700

    Fix warning message

--- google_sheets.py
+++ google_sheets.py
@@ -96,7 +96,7 @@ def update_canadians_sheet():
     summary_data = [
         ['Canadian Baseball Network', '', '', '', f'Last updated: {now.strftime("%B %d, %Y")}'],
         ['Pete Berryman', '', '', '', '' if str(now.year) == config['YEAR'] else (u'\u26A0' + ' If a player is missing from this list, it could be because')],
-        ['', '', '', '', '' if now.year == config['YEAR'] else f'many schools have not yet posted their {config["YEAR"]} rosters.'],
+        ['', '', '', '', '' if str(now.year) == config['YEAR'] else f'many schools have not yet posted their {config["YEAR"]} rosters.'],
         ['Total', f'{len(players_df.index)} players', '', '', ''],
         blank_row
     ]
