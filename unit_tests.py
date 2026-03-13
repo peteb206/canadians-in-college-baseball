bcee7c114b075ecc2abaa5a1a2abce31a661e852
Pete Berryman <70476220+peteb206@users.noreply.github.com>
2026-03-13 17:45:37 -0500

    Try fix for 405

--- unit_tests.py
+++ unit_tests.py
@@ -5,11 +5,15 @@ import time
 
 urls_to_try = [
     'https://smcgaels.com/sports/baseball/roster/2026',
+    'https://athletics.sierracollege.edu',
     'https://athletics.sierracollege.edu/sports/bsb/2025-26/roster',
+    'https://stats.ncaa.org',
     'https://stats.ncaa.org/players/11216497',
     'https://njcaastats.prestosports.com/sports/bsb/2025-26/div1/players/williammerrikin4d53?serverSide',
     'https://naiastats.prestosports.com/sports/bsb/2025-26/players/tysonkilbreathnkcr',
+    'https://nwacsports.com',
     'https://nwacsports.com/sports/bsb/2025-26/players/domenicdipalmazyn2',
+    'https://www.cccaasports.org',
     'https://www.cccaasports.org/sports/bsb/2025-26/players/declanbarrymci9?serverSide',
     'https://uscaa.prestosports.com/sports/bsb/2025-26/players/dawsonbabineaujytv'
 ]
